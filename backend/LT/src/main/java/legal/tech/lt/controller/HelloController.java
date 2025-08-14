ccf82fb83acc571ce3c1fad830cb3da1390d2a58
LOUTFI <loutfi.mohamedlm@gmail.com>
2025-08-15 00:44:33 +0100

    backend
    default repositories

---
+++ backend/LT/src/main/java/legal/tech/lt/controller/HelloController.java
@@ -0,0 +1,12 @@
+package legal.tech.lt.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Bonjour LegalTech 🚀";
+}}
---
+++ backend/LT/src/test/java/legal/tech/lt/LtApplicationTests.java
@@ -0,0 +1,13 @@
+package legal.tech.lt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
